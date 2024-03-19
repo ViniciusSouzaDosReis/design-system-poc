0a4671b2570fa0c9006a81d153b31d2a012cd942
Vinicius Reis <vinireis010305@gmail.com>
2024-03-19 13:01:49 -0300

    config react package

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@poc-design-system-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@poc-design-system-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.gray100 }}>Ola</h1>
+}
